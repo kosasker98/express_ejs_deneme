bb5bad5d43a13c89308c95840cda6348b70a8505
İbrahim Akçakoca <ibrahimmakcakoca@gmail.com>
2023-07-19 12:37:52 +0300

    deneme nodejs

---
+++ index.js
@@ -0,0 +1,37 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer((req,res) => {
+
+if (req.url == "/") {
+    fs.readFile("index.html", (err,html) => {
+
+        res.write(html);
+        res.end();
+
+    });
+}
+else if (req.url == "/urunler") {
+    fs.readFile("urunler.html", (err,html) => {
+
+        res.write(html);
+        res.end();
+
+    });
+}
+else {
+fs.readFile("404.html",(err,html) => {
+
+    res.write(html);   
+    res.end();
+
+});}
+
+});
+
+server.listen(8080, () => {
+
+    console.log("server devrede ");
+    console.log(8+2);
+    console.log(5);
+});
